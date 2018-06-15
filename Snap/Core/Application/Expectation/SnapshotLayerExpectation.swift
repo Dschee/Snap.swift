04be68969f2e4c96381aaac6ce7abea26e50dcee
Cihat Gündüz <cihat.guenduez@jamitlabs.com>
2018-06-15 12:10:59 +0200

    Fix issues with Swift 4.1 global overload

--- Snap/Core/Application/Expectation/SnapshotLayerExpectation.swift
+++ Snap/Core/Application/Expectation/SnapshotLayerExpectation.swift
@@ -4,13 +4,18 @@ import XCTest
 // MARK: - XCTestCase + CALayer Expectation
 
 extension XCTestCase {
+  @available(swift, obsoleted: 4.1, renamed: "expectSnapshot")
   public func expect(_ layer: CALayer, function: String = #function, file: String = #file ) -> LayerMatcher {
+    return self.expectSnapshot(layer, function: function, file: file)
+  }
+
+  public func expectSnapshot(_ layer: CALayer, function: String = #function, file: String = #file ) -> LayerMatcher {
     let testTarget = TestTarget(
       function: function,
       file: file
     )
     layer.layoutIfNeeded()
-    
+
     return resolver.makeMatcher(
       with: layer,
       isRecording: isRecording,
--- Snap/Core/Application/Expectation/SnapshotViewExpectation.swift
+++ Snap/Core/Application/Expectation/SnapshotViewExpectation.swift
@@ -4,21 +4,31 @@ import XCTest
 // MARK: - XCTestCase + View Expectation
 
 extension XCTestCase {
+  @available(swift, obsoleted: 4.1, renamed: "expectSnapshot")
   public func expect(_ view: UIView, function: String = #function, file: String = #file ) -> ViewMatcher {
+    return self.expectSnapshot(view, function: function, file: file)
+  }
+
+  @available(swift, obsoleted: 4.1, renamed: "expectSnapshot")
+  public func expect(_ viewController: UIViewController, function: String = #function, file: String = #file ) -> ViewMatcher {
+    return self.expectSnapshot(viewController.view, function: function, file: file)
+  }
+
+  public func expectSnapshot(_ view: UIView, function: String = #function, file: String = #file ) -> ViewMatcher {
     let testTarget = TestTarget(
       function: function,
       file: file
     )
     view.layoutIfNeeded()
-    
+
     return resolver.makeMatcher(
       with: view,
       isRecording: isRecording,
       tesTarget: testTarget
     )
   }
-  
-  public func expect(_ viewController: UIViewController, function: String = #function, file: String = #file ) -> ViewMatcher {
-    return self.expect(viewController.view, function: function, file: file)
+
+  public func expectSnapshot(_ viewController: UIViewController, function: String = #function, file: String = #file ) -> ViewMatcher {
+    return self.expectSnapshot(viewController.view, function: function, file: file)
   }
 }
